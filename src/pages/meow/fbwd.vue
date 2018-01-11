caee4c8c2333f1f49ecd1c72a44a9d470be838a8
cochlea01 <greatmeng01@126.com>
2018-01-11 20:38:20 +0800

    问答 step1

--- src/pages/meow/fbwd.vue
+++ src/pages/meow/fbwd.vue
@@ -4,15 +4,15 @@
     <div class="header">
       <div class="header-left"@click="goback"><img src="../../../static/images/back.png" /></div>
       <span>发布问题</span>
-      <div class="header-right">完成</div>
+      <div class="header-right" @click="submit">完成</div>
     </div>
     <div class="set-content">
       <div class="xgnc">
-        <div class="left"><input type="text" maxlength="20"placeholder="标题（最多20字）" class="btname"></div>
+        <div class="left"><input v-model="question.title" type="text" maxlength="20" placeholder="标题（最多20字）" class="btname" ref="title"></div>
       </div>
       <!--留言-->
       <div class="pc-shuru">
-        <textarea class="area" maxlength="300" placeholder="请输入问题描述"></textarea>
+        <textarea class="area" ref="area" maxlength="300" placeholder="请输入问题描述" v-model="question.description"></textarea>
         <p class="xianzhi"><span class="zs">300</span>/<span>300</span></p>
       </div>
       <!--图片上传-->
@@ -22,42 +22,104 @@
         </div>
       </div>
     </div>
+    <toast v-model="toastShow" type="text" :text="toastText" width="4em"></toast>
   </div>
 </template>
 
 <script>
-  export default {
-    data: function () {
-      return {
+    import {Toast} from 'vux'
+    export default {
+        components:{
+            Toast
+        },
+        data: function () {
+            return {
+                toastShow:false,
+                toastText:'sss',
+                question:{
+                    title:'',
+                    description:'',
+                    user_id:null,
+                    comments:0,
+                    collection:0,
+                    like:0,
+                    type:5//问答,
+                },
+            }
+        },
+        created(){
+            var _slef = this;
+            var user = common.getObjStorage("userInfo") || {};
+            if( !common.isNull(user._id) ){
+                _slef.question.user_id = user._id;
+            }
+        },
+        mounted: function () {
+            this. wzxz()
+        },
+        methods: {
+            goback () {
+                this.$router.goBack()
+            },
+            toUrl: function (pagename) {
+                this.$router.push({name: pagename})
+            },
+            //留言字数限制
+            wzxz(){
+                $(".area").bind("input propertychange",function(){
+                    var curlength=$(".area").val().length;
+                    if(curlength>300){
+                    var num=$(".area").val().substr(0,300);
+                    $('.area').val(num);
 
-      }
-    },
-    mounted: function () {
-      this. wzxz()
-    },
-    methods: {
-      goback () {
-        this.$router.goBack()
-      },
-      toUrl: function (pagename) {
-        this.$router.push({name: pagename})
-      },
-      //留言字数限制
-      wzxz(){
-        $(".area").bind("input propertychange",function(){
-          var curlength=$(".area").val().length;
-          if(curlength>300){
-            var num=$(".area").val().substr(0,300);
-            $('.area').val(num);
-            alert("您输入的字数已超出");
-          }
-          else{
-            $(".zs").text(300-$(".area").val().length)
-          }
-        })
-      }
+                    alert("您输入的字数已超出");
+                    }
+                    else{
+                        $(".zs").text(300-$(".area").val().length)
+                    }
+                })
+            },
+            showToast(msg){
+                this.toastShow = true;
+                this.toastText = msg;
+            },
+            submit(){
+                let _self = this;
+                let title = _self.$refs.title.value;
+                let area = _self.$refs.area.value;
+                if(common.isNull(title)) {
+                    _self.showToast("请填写标题");
+                    return;
+                }
+                if (common.isNull(area)) {
+                    _self.showToast("请填写提问内容");
+                    return;
+                }
+
+
+                let params = {
+                    interfaceId:common.interfaceIds.insertData,
+                    coll:common.collections.personalChw,               
+                    data:_self.question,
+                };
+                _self.$axios.post('/mongoApi', {
+                    params: params
+                    }, response => {
+                            console.log(response)
+                            var data = response.data;
+                            if( data && response.code == '200' ){
+                                _self.showToast("发布成功！");
+
+                                setTimeout(function () {
+                                _self.goback();
+                                },1000)
+                            }else{
+                                _self.showToast("发布失败！");
+                            }
+                        })
+            }
+        }
     }
-  }
 </script>
 <style scoped>
   @import '../../../static/css/meow/fbwd.css';
--- src/pages/meow/shouchang-weida.vue
+++ src/pages/meow/shouchang-weida.vue
@@ -2,40 +2,18 @@
   <div>
     <!--头部导航-->
     <div class="header">
-      <div class="header-left"@click="goback"><img src="../../../static/images/back.png" /></div>
+      <div class="header-left" @click="goback"><img src="../../../static/images/back.png" /></div>
       <span>问答</span>
-      <div class="header-right"@click="toUrl('fbwd')">提问</div>
+      <div class="header-right" @click="toUrl('fbwd')">提问</div>
     </div>
     <!--问答专辑-->
-    <div class="weida-list">
-      <div class="weida"@click="toUrl('wdxq')">
+    <div class="weida-list" v-for="item in QAList" :key="common.uuid()">
+      <div class="weida" @click="toUrl('wdxq')">
         <div class="wd-top">
           <div class="touxiang">
             <img src="../../../static/images/bj.jpg"/>
           </div>
-          <p class="nicheng">昵称</p>
-        </div>
-        <div class="tupian">
-          <img src="../../../static/images/bj.jpg"/>
-        </div>
-        <div class="neirong">
-          <div class="piapti">
-            如果你无法简洁的表达你的想法，那只说明你还不够了解它。- 阿尔伯特·爱因斯坦?
-          </div>
-          <div class="jieshao">
-            如果你无法简洁的表达你的想法，那只说明你还不够了解它阿尔伯特·爱因斯坦如果你无法简洁的表达你的想法，那只说明你还不够了解它。- 阿尔伯特·爱因斯坦阿尔伯特
-          </div>
-        </div>
-        <div class="pingjia">
-          <span>100</span>赞同 · <span>100</span>评论
-        </div>
-      </div>
-      <div class="weida">
-        <div class="wd-top">
-          <div class="touxiang">
-            <img src="../../../static/images/bj.jpg"/>
-          </div>
-          <p class="nicheng">昵称</p>
+          <p class="nicheng">item.</p>
         </div>
         <div class="tupian">
           <img src="../../../static/images/bj.jpg"/>
@@ -60,16 +38,55 @@
   export default {
     data: function () {
       return {
-
+          user_id:null,
+          QAList:[]
       }
     },
     methods: {
-      goback () {
-        this.$router.goBack()
-      },
-      toUrl: function (pagename) {
-        this.$router.push({name: pagename})
-      },
+        goback () {
+            this.$router.goBack()
+        },
+        toUrl: function (pagename) {
+            this.$router.push({name: pagename})
+        },
+        loadData(){
+            console.log('this is  loadData');
+            let _self = this;
+            if( common.isNull(_self.user_id) ){
+                alert(1);
+                return;
+            }
+            let params = {
+                interfaceId: common.interfaceIds.getPersonalChw,
+                pageNo:0,
+                pageSize:3,
+                where:{
+                    user_id: _self.user_id,
+                    type: 5//问答
+                }
+            }
+            _self.$axios.post('/mongoApi', {
+                params: params
+                }, response => {
+                    console.log(response);
+                    var data = response.data
+                    if (data) {
+                        _self.QAList = data.chws;
+                        console.log(data);
+                    }
+                });
+        }
+    },
+    created(){
+        var _slef = this;
+        var user = common.getObjStorage("userInfo") || {};
+        if( !common.isNull(user._id) ){
+            _slef.userInfo = user;
+            _slef.user_id = user._id;
+        }
+        // this.loadData();
+        const obj = Object.create({},{p:{value:42}});
+        console.log(Object.values(obj));
     }
   }
 </script>
