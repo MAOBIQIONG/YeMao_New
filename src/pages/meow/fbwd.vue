4a405f050462462ac3251c0f04b240bf7113d25f
cochlea01 <greatmeng01@126.com>
2018-01-23 20:16:01 +0800

    发布订单修改

--- src/pages/meow/fbwd.vue
+++ src/pages/meow/fbwd.vue
@@ -104,6 +104,8 @@
             },
             submit(){
                 let _self = this;
+                let title = _self.$refs.title.value;
+                let area = _self.$refs.area.value;
                 if( common.isNull(_self.params.user_id) ){
                     _self.showToast("未成功获取用户信息!");
                     return;
@@ -129,9 +131,6 @@
             },
             submit2(){
                 let _self = this;
-                let title = _self.$refs.title.value;
-                let area = _self.$refs.area.value;
-
                 let params = {
                     interfaceId:common.interfaceIds.insertData,
                     coll:common.collections.personalChw,
