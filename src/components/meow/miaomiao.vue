1bc3de1a9e9f3347c7469de3d7d06e8d69211db2
cochlea01 <greatmeng01@126.com>
2018-03-13 17:40:18 +0800

    自定义previewer2组件

--- src/components/meow/miaomiao.vue
+++ src/components/meow/miaomiao.vue
@@ -77,7 +77,7 @@
             </div>
             <!--<div v-if="item.prevImgs && item.prevImgs.length > 0" >-->
               <div v-transfer-dom>
-                <previewer :list="item.prevImgs" ref="previewer" :options="options"></previewer>
+                <previewer2 :list="item.prevImgs" ref="previewer" :options="options"></previewer2>
               </div>
             <!--</div>-->
           </div>
@@ -95,6 +95,7 @@
 
 <script>
   import { Previewer, TransferDom, Scroller, LoadMore, Toast } from 'vux'
+  import Previewer2 from '@/components/meow/previewer2' 
   import store from '@/vuex/store'
   export default {
     directives: {
@@ -104,7 +105,8 @@
       Previewer,
       Scroller,
       LoadMore,
-      Toast
+      Toast,
+      Previewer2,
     },
     store,
     data() {
