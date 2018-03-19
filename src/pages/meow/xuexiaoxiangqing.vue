61f7fabe9308d5a234a6b0c99224de4bfe60e9bb
wentt <769875543@qq.com>
2018-03-19 11:15:40 +0800

    1、校友会详情。

--- src/pages/meow/xuexiaoxiangqing.vue
+++ src/pages/meow/xuexiaoxiangqing.vue
@@ -9,10 +9,7 @@
     <!--学校详情-->
     <div class="content content-p">
       <div class="xxmc">
-        <div class="touxiang" :style="{backgroundImage:`url(${checkAvatar(alumni.school_logo)})`}">
-          <!-- <img src="../../../static/images/bj.jpg" /> -->
-          
-        </div>
+        <div class="touxiang" :style="{backgroundImage:`url(${checkAvatar(alumni.school_logo)})`}"></div>
         <div class="mingcheng">
           <div class="xuexiao">{{alumni.school_name}}</div>
           <!--  v-tap="{methods:toUrl2,pagename:'groupchat',query:{id:'343647591',name:'内部群001',img:'./static/images/logo.png'}}" -->
@@ -28,12 +25,16 @@
       <div class="liebiao">
         <div class="list">
           <div class="xingxi">管理员</div>
-          <div class="guanliyuan">
+          <div class="guanliyuan" v-tap="{methods:toDetails,id:alumni.user_id}">
             <div class="gly">
-              <div class="touxiang">
+              <div class="touxiang" v-if="alumni.user!=null&&alumni.user!=undefined">
+                <img :src="checkAvatar(alumni.user.img)" />
+              </div>
+              <div class="touxiang" v-else>
                 <img src="../../../static/images/bj.jpg" />
               </div>
-              <p class="nicheng">昵称</p>
+              <p class="nicheng" v-if="alumni.user!=null&&alumni.user!=undefined">{{alumni.user.user_name}}</p>
+              <p class="nicheng" v-else>昵称</p>
             </div>
           </div>
           <!--<div class="list-right"><img src="../../../static/images/jiangou.png"></div>-->
@@ -55,11 +56,9 @@
     </div>
     <loading :show="loadingShow" text="加载中"></loading>
     <div v-transfer-dom>
-        <confirm v-model="confirmShow"
-            @on-confirm = "compOnConfirm()"
-        >
+      <confirm v-model="confirmShow" @on-confirm = "compOnConfirm()">
         <p style="text-align:center;">{{confirmMsg}}</p>
-        </confirm>
+      </confirm>
     </div>
   </div>
 </template>
@@ -68,11 +67,11 @@
 import {Loading,Confirm,TransferDomDirective as TransferDom} from 'vux';
   export default {
     components:{
-        Loading,
-        Confirm
+      Loading,
+      Confirm
     },
     directives: {
-        TransferDom
+      TransferDom
     },
     data: function () {
       return {
@@ -80,135 +79,144 @@ import {Loading,Confirm,TransferDomDirective as TransferDom} from 'vux';
         alumni:{},
         loadingShow:false,
         confirmShow:false,
-        confirmType:'',//addAlumnis 校友会添加成员
+        confirmType:'', //addAlumnis 校友会添加成员
         confirmMsg:'',
         user_id:null,
         alumniList:[],
       }
     },
     watch:{
-        loaded:{
-            handler(val,oldval){
-                if(val == true){
-                    this.setData(this.data);
-                }
-            }
+      loaded:{
+        handler(val,oldval){
+          if(val == true){
+            this.setData(this.data);
+          }
         }
+      }
     },
     created(){
-        this.user = common.getObjStorage("userInfo") || {};
-        let itemId = this.$route.query.id;
-        if(itemId){
-            this.itemId = itemId;
-            this.requestData();
-        }
+      console.log("xxxq created:")
+      this.user = common.getObjStorage("userInfo") || {};
+      let itemId = this.$route.query.id;
+      if(itemId){
+        this.itemId = itemId;
+        this.requestData();
+      }
     },
     methods: {
-        goback () {
-            this.$router.goBack()
-        },
-        // 头像
-        checkAvatar (path) {
-            return common.getAvatar(path)
-        },
-        checkImg(path){
-            return common.getDefultImg(path);
-        },       
-        toUrl: function (pagename) {
-            this.$router.push({name: pagename})
-        },
-        toUrl2: function (params) {
-          console.log("params.pagename:"+params.pagename)
-          this.$router.push({name: params.pagename, query: params.query})
-        },
-        toUrlAfterLogin(params){
-            var _self = this;
-            if( !common.isNull(_self.user._id) ){
-                _self.toUrl2(params);
-            } else {
-                console.log('没有获取用户信息');
-                _self.toUrl2({pagename:"login"});
-            }
-         },
-        toPhoneList(param){
-            var _self = this;
-            if( !common.isNull(_self.user._id) ){
-                 _self.$router.push({name:'tongxunluhaoyou',query:{aa_id:param.aa_id}})
-            } else {
-                console.log('没有获取用户信息');
-                _self.toUrl2({pagename:"login"});
-            }
-        },
+      goback () {
+        this.$router.goBack()
+      },
+      // 头像
+      checkAvatar (path) {
+        return common.getAvatar(path)
+      },
+      checkImg(path){
+        return common.getDefultImg(path);
+      },
+      toUrl: function (pagename) {
+        this.$router.push({name: pagename})
+      },
+      toUrl2: function (params) {
+        console.log("params.pagename:"+params.pagename)
+        this.$router.push({name: params.pagename, query: params.query})
+      },
+      // 详情页
+      toDetails (param) {
+        var _self = this;
+        if( !common.isNull(_self.user._id) && _self.user._id==param.id ){
+          _self.$router.push({name: 'shejishigerenzhongxin'});
+        } else {
+          _self.$router.push({name: 'sjszxxq', query: {id: param.id}});
+        }
+      },
+      toUrlAfterLogin(params){
+        var _self = this;
+        if( !common.isNull(_self.user._id) ){
+          _self.toUrl2(params);
+        } else {
+          console.log('没有获取用户信息');
+          _self.toUrl2({pagename:"login"});
+        }
+      },
+      toPhoneList(param){
+        var _self = this;
+        if( !common.isNull(_self.user._id) ){
+          _self.$router.push({name:'tongxunluhaoyou',query:{aa_id:param.aa_id}})
+        } else {
+          console.log('没有获取用户信息');
+          _self.toUrl2({pagename:"login"});
+        }
+      },
         // 头像
       checkAvatar (path) {
         return common.getAvatar(path)
       },
       checkImg(path){
         return common.getDefultImg(path);
-      },    
-        requestData(){
-            let _self = this;
-            _self.loadingShow = true;
-            setTimeout(()=>{_self.loadingShow = false},2000);
-            let params = {
-                interfaceId:common.interfaceIds.queryAlumniById,
-                _id:_self.itemId
-            }
-            _self.$axios.post('/mongoApi', {
-                params: params
-                }, response => {
-                    console.log(response);
-                    let data = response.data.alumni
-                    _self.data = data;
-                    if (data) {
-                        _self.loaded = true;
-
-                    } else {
-                        console.log('noData');
-                    }
-                });
-            this.getAlumnis();
-        },
-        getAlumnis(){
-            let _self = this;
-            let params = {
-                interfaceId:common.interfaceIds.getAlumnis,
-                aa_id:_self.itemId
-            }
-            _self.$axios.post('/mongoApi', {
-                params: params
-                }, response => {
-                    let data = response.data.alumnis
-                    common.setStorage('alumnisList',data);
-                    if (data) {
-                        _self.alumniList = data;
-                    } else {
-                        console.log('noData');
-                    }
-                });
-        },
-        setData(data){
-            console.log('set Data');
-            console.log(data);
-            let _self = this;
-            _self.loadingShow = false;
-            _self.alumni = data;
-            console.log('数据设置完成');
-        },
-        showConfirm(params){
-            this.confirmShow = true;
-            this.user_id = params.uid;
-            this.confirmType = params.type
-            if(params.type == "addAlumnis"){
-                this.confirmMsg = "校友会添加该用户吗？"
-            } 
-        },
-        compOnConfirm(){
-            if(this.confirmType=="addAlumnis"){
-                this.addAlumnis();
-            }
-        },
-        addAlumnis(){}
+      },
+      requestData(){
+        let _self = this;
+        _self.loadingShow = true;
+        setTimeout(()=>{_self.loadingShow = false},2000);
+        let params = {
+          interfaceId:common.interfaceIds.queryAlumniById,
+          _id:_self.itemId
+        }
+        _self.$axios.post('/mongoApi', {
+            params: params
+        }, response => {
+          // console.log(response);
+          let data = response.data.alumni;
+          _self.data = data;
+          if (data) {
+            _self.loaded = true;
+            _self.getAlumnis();
+          } else {
+            console.log('noData');
+          }
+        });
+      },
+      getAlumnis(){
+        let _self = this;
+        let params = {
+          interfaceId:common.interfaceIds.getAlumnis,
+          aa_id:_self.itemId
+        }
+        _self.$axios.post('/mongoApi', {
+          params: params
+        }, response => {
+          let data = response.data.alumnis
+          common.setStorage('alumnisList',data);
+          if (data) {
+            _self.alumniList = data;
+          } else {
+            console.log('noData');
+          }
+        });
+      },
+      setData(data){
+        console.log('set Data');
+        console.log(data);
+        let _self = this;
+        _self.loadingShow = false;
+        _self.alumni = data;
+        console.log('数据设置完成');
+      },
+      showConfirm(params){
+        this.confirmShow = true;
+        this.user_id = params.uid;
+        this.confirmType = params.type
+        if(params.type == "addAlumnis"){
+          this.confirmMsg = "校友会添加该用户吗？"
+        }
+      },
+      compOnConfirm(){
+        if(this.confirmType=="addAlumnis"){
+          this.addAlumnis();
+        }
+      },
+      addAlumnis(){}
     }
   }
 </script>
