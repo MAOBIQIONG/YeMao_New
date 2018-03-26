334e341f469b8e217244256cf13a7b2849514d85
wentt <769875543@qq.com>
2018-03-26 10:15:45 +0800

    1、分享。

--- static/js/es6/share.js
+++ static/js/es6/share.js
@@ -26,7 +26,7 @@ const myshare ={
         }
       }
     }, function() {
-      console.log("获取分享服务列表失败");
+      alert("获取分享服务列表失败");
     });
   },
 
@@ -37,15 +37,13 @@ const myshare ={
       return;
     }
     // 初始化分享
-    console.log("shares:"+JSON.stringify(myshare.shares))
     if( JSON.stringify(myshare.shares) === "{}" ){
       myshare.getShares();
     }
-    console.log("shares:"+JSON.stringify(myshare.shares))
     if( params ){
       if( myshare.isNull(params.href) || myshare.isNull(params.title) ||
           myshare.isNull(params.content) || myshare.isNull(params.thumbs) ){
-        console.log("缺少分享参数!");
+        alert("缺少分享参数!");
         return;
       }
       // 分享参数
@@ -63,7 +61,7 @@ const myshare ={
       // 发起分享
       myshare.showActionSheet();
     }else{
-      console.log("分享参数错误!")
+      alert("分享参数错误!")
     }
   },
 
@@ -98,7 +96,7 @@ const myshare ={
           share.authorize(function() {
             myshare.shareMessage(share, ids[i - 1].ex);
           }, function(e) {
-            console.log("认证授权失败：" + e.code + " - " + e.message);
+            alert("认证授权失败：" + e.code + " - " + e.message);
           });
         }
       }
@@ -108,13 +106,15 @@ const myshare ={
   shareMessage: function (share, ex) {
     myshare.msg.extra = { scene: ex };
     share.send(myshare.msg, function() {
-      console.log("分享到\"" + share.description + "\"成功！ ");
+      // console.log("分享到\"" + share.description + "\"成功！ ");
+      alert("分享到\"" + share.description + "\"成功！ ");
       /**
        * 分享成功,可进行其他操作
        * **/
       myshare.callback();
     }, function(e) {
-      console.log("分享到\"" + share.description + "\"失败: " + e.code + " - " + e.message);
+      // console.log("分享到\"" + share.description + "\"失败: " + e.code + " - " + e.message);
+      alert("分享到\"" + share.description + "\"失败: " + e.code + " - " + e.message);
     });
   }
 
