7fc0b2def038cd526d32f504601bbde9e459cbca
wentt <769875543@qq.com>
2018-07-09 11:00:53 +0800

    夜猫优化-20180709:验证码发送成功,回调失败.

--- static/js/es6/interface.js
+++ static/js/es6/interface.js
@@ -6,12 +6,7 @@ const interfaces ={
   getVerifyCode:function(params,callback){
     var qsParams = Qs.stringify(params)// 解决后台接收参数错误问题
     webapi.post('appSend/sendmessage', qsParams, function (result) {
-      var data = {};
-      console.log("idString:"+common.isString(result))
-      if( result ){
-        data = JSON.parse(result);
-      }
-      callback(data)
+      callback(result)
     })
   },
 }
